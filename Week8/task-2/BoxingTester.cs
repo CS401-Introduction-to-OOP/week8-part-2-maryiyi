f193d5fd7d2e02e3c8a7171d842e644bba7b0cc4
maria <mary.horodyska@gmail.com>
2026-03-28 16:25:59 +0200

    2 stage

--- Week8/task-2/BoxingTester.cs
+++ Week8/task-2/BoxingTester.cs
@@ -1,6 +1,16 @@
 namespace Week8.task_2;
 
-public class BoxingTester
+public static class BoxingTester
 {
-    
+    public static void Test()
+    {
+        int a = 10; // creating new int
+        
+        object b = a; //coping from stack to heap
+        int c = (int)b; //pulling value from obj in heap to stack as int
+        
+        Console.WriteLine($"origin int a: {a}");
+        Console.WriteLine($"boxed object b: {b}");
+        Console.WriteLine($"unboxed int c: {c}");
+    }
 }
