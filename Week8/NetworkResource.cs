09bdbff07cb7fc204e012d2d64d49fcf095f130c
maria <mary.horodyska@gmail.com>
2026-03-28 16:08:54 +0200

    2 stage

---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,24 @@
+namespace Week8;
+
+public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+    public override void Close()
+    {
+        if (IsOpen)
+        {
+            IsOpen = false;
+        }
+    }
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine($"Close: {Name}");
+    }
+}
